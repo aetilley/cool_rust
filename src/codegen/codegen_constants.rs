823d11b810f2220fe9ecd103098c4b9d70b06fed
arthur <tilley@fastmail.com>
2025-03-28 13:01:31 -0700

    Cleanup

--- src/codegen/codegen_constants.rs
+++ src/codegen/codegen_constants.rs
@@ -1,38 +1,32 @@
 use crate::symbol::Sym;
 use std::hash::{DefaultHasher, Hash, Hasher};
 
-// Type Names
+// Globals
+pub const VTABLE_MASTER_VECTOR: &str = "vtable_master_vector";
+pub const PARENT_VECTOR: &str = "parent_vector";
+pub const TYPE_NAME_VECTOR: &str = "type_name_vector";
+pub const STRUCT_SIZE_TABLE: &str = "struct_size_table";
+
+// Struct offsets
+pub const OBJECT_PREFIX_SIZE: u32 = 1;
+pub const CLASS_ID_IND: u32 = 0;
+pub const INT_VAL_IND: u32 = 1;
+pub const BOOL_VAL_IND: u32 = 1;
+pub const STRING_LEN_IND: u32 = 1;
+pub const STRING_CONTENT_IND: u32 = 2;
+
+// Numeric Parameters
+pub const MAX_IN_STRING_LEN: u64 = 100;
+pub const MEMCPY_ALIGNMENT: u32 = 8;
+
+// Native type names
 pub const BOOL: &str = "Bool";
 pub const IO: &str = "IO";
 pub const OBJECT: &str = "Object";
 pub const INT: &str = "Int";
 pub const STRING: &str = "String";
 
-// Method Names
-pub const SELF: &str = "self";
-pub const INIT: &str = "init";
-pub const TRUE: &str = "true";
-pub const FALSE: &str = "false";
-
-pub const CLASS_ID_IND: u32 = 0;
-pub const OBJECT_PREFIX_SIZE: u32 = 1;
-
-pub const VTABLE_MASTER_VECTOR: &str = "vtable_master_vector";
-pub const PARENT_VECTOR: &str = "parent_vector";
-pub const TYPE_NAME_VECTOR: &str = "type_name_vector";
-pub const STRUCT_SIZE_TABLE: &str = "struct_size_table";
-
-pub const INT_VAL_IND: u32 = 1;
-pub const BOOL_VAL_IND: u32 = 1;
-pub const STRING_LEN_IND: u32 = 1;
-pub const STRING_CONTENT_IND: u32 = 2;
-
-// Should maybe use %c to read whitespace.
-pub const STRING_FORMATTER: &str = "%s\0";
-pub const INT_FORMATTER: &str = "%d\0";
-pub const MAX_IN_STRING_LEN: u64 = 100;
-pub const MEMCPY_ALIGNMENT: u32 = 8;
-
+// Native method names
 pub const OUT_STRING: &str = "out_string";
 pub const IN_STRING: &str = "in_string";
 pub const OUT_INT: &str = "out_int";
@@ -48,8 +42,13 @@ pub const ABORT: &str = "abort";
 pub const COPY: &str = "copy";
 pub const TYPE_NAME: &str = "type_name";
 
+// Misc
 pub const ENTRY: &str = "main";
+pub const SELF: &str = "self";
+pub const TRUE: &str = "true";
+pub const FALSE: &str = "false";
 
+// Builders
 pub fn method_ref(cls_name: &Sym, method_name: &Sym) -> String {
     format!("{}.{}", cls_name, method_name)
 }
